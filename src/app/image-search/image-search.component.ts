3c6cdcdc40c6d82163b81f58167c0630484782ea
Alvin Allen <alaj6625@gmail.com>
2017-07-10 12:12:39 -0500

    updated component

--- src/app/image-search/image-search.component.ts
+++ src/app/image-search/image-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ImageService } from '../shared/image.service';
 @Component({
   selector: 'app-image-search',
   templateUrl: './image-search.component.html',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _imageService : ImageService) { }
 
   ngOnInit() {
+    console.log('Init for image-search');
   }
 
 }
